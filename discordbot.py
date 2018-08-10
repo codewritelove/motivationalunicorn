9feadbf5c146bc607753fd4afe786641758d9daf
codewritelove <42278916+codewritelove@users.noreply.github.com>
2018-08-10 16:15:39 -0400

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,9 +15,8 @@ async def motivational_unicorn ():
         'Virtual huggie!',
         'Who is the best? You are!',
         'YOU CAN DO IT!',
-        'You are so close! You got this!',
         'You will never know if you do not try',
-        'You have come so far! Do not give up!',
+        'You have come so far!',
         'You got this!',
         'Hang in there!',
         'You are unstoppable!',
@@ -25,7 +24,7 @@ async def motivational_unicorn ():
         'I am so proud of you!',
         'You are creating something out of nothing and that is a huge deal!',
         'No matter what happens, you started something big and no one can take that away!',
-     
+        'Do not give up!',
         
     ]
     await client.say(random.choice (possible_responses) )
