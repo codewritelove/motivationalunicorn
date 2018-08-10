8f8d67f57e45460bc9c8bff017baf535d34261b6
codewritelove <42278916+codewritelove@users.noreply.github.com>
2018-08-10 13:27:30 -0400

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,30 @@
+import random
+
+from discord.ext.commands import Bot
+BOT_PREFIX = ("?", "!")
+
+TOKEN="NDY2NzU3MzU0MDA5MTMzMDk2.Dk5KRQ.wceTI9pweETfGwo9G-I0Ya4Qf_g"
+
+client=Bot(command_prefix=BOT_PREFIX)
+
+@client.command (name='motivationalunicorn',
+                 description="Motivational quotes.",
+                  brief="A unicorn that helps you navigate through life.")
+async def motivational_unicorn ():
+    possible_responses = [
+        'Virtual huggie!',
+        'Who is the best? You are!',
+        'YOU CAN DO IT!',
+        'I am so proud of you! You are creating something out of nothing and that is a huge deal! No matter what happens, you started something big and no one can take that away.',
+        'You are so close! You got this!',
+        'You will never know if you do not try',
+        'You have come so far! Do not give up!',
+
+
+
+    ]
+    await client.say(random.choice (possible_responses) )
+
+
+
+client.run(TOKEN)
